2b06adc6e6aff4029a1c634f3780d29a970f64d1
Patrick Mackow <patrickmackow@gmail.com>
2021-09-03 13:24:17 -0400

    Add more test patterns

--- src/utils/matcher.test.ts
+++ src/utils/matcher.test.ts
@@ -3,14 +3,24 @@ import matcher from "./matcher";
 const url =
   "https://medium.com/c-sharp-progarmming/create-your-own-web-scraper-in-c-in-just-a-few-minutes-c42649adda8";
 
-test("matcher matches valid pattern", () => {
-  const valid = "*://medium.com/*";
+describe("matcher matches valid pattern", () => {
+  const valid = [
+    "*://medium.com/*",
+    "http*://medium.com/*",
+    "https://medium.com/c-sharp-progarmming/create-your-own-web-scraper-in-c-in-just-a-few-minutes-c42649adda8",
+    "*",
+    "**",
+  ];
 
-  expect(matcher(valid, url)).toBe(true);
+  test.each(valid)("%s", (pattern) => {
+    expect(matcher(pattern, url)).toBe(true);
+  });
 });
 
-test("matcher doesn't match invalid pattern", () => {
-  const invalid = "*://medium.com";
+describe("matcher doesn't match invalid pattern", () => {
+  const invalid = ["*://medium.com", "http://*", ""];
 
-  expect(matcher(invalid, url)).toBe(false);
+  test.each(invalid)("%s", (pattern) => {
+    expect(matcher(pattern, url)).toBe(false);
+  });
 });
