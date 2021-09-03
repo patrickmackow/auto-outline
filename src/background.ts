284ba90a46279332c13a4bcd9876626e5e0eda2f
Patrick Mackow <patrickmackow@gmail.com>
2021-09-03 10:53:41 -0400

    Redirect url to outline if it matches a pattern

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,34 @@
 import * as browser from "webextension-polyfill";
+import matcher from "./utils/matcher";
 
-var urls = [];
+var patterns = [] as string[];
 
-browser.storage.local.get().then((data) => (urls = data.urls ?? []));
+function redirectMatchUrls(
+  details: browser.WebRequest.OnBeforeRequestDetailsType
+): browser.WebRequest.BlockingResponse | void {
+  const { url } = details;
 
-browser.storage.onChanged.addListener((changes) => updateUrls(changes));
-
-function updateUrls(changes: Record<string, browser.Storage.StorageChange>) {
-  urls = changes.url?.newValue ?? [];
-  console.log(urls);
+  if (!url.startsWith("https://outline.com") && matchesAny(patterns, url)) {
+    return { redirectUrl: `https://outline.com/${url}` };
+  }
 }
+
+function matchesAny(patterns: string[], url: string) {
+  return patterns.some((pattern) => matcher(pattern, url));
+}
+
+(function init() {
+  browser.storage.local.get().then((data) => {
+    patterns = data.patterns;
+  });
+
+  browser.storage.onChanged.addListener((changes) => {
+    patterns = changes.patterns.newValue || [];
+  });
+
+  browser.webRequest.onBeforeRequest.addListener(
+    redirectMatchUrls,
+    { urls: ["<all_urls>"] },
+    ["blocking"]
+  );
+})();
